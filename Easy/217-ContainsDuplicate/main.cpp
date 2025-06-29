1a79c1c3f017c3fae3867ef92abcd1645aefcdf8
IsakOswald <isak.oswald.cs@gmail.com>
2025-06-29 21:43:34 +1000

    Added comments

--- Easy/217-ContainsDuplicate/main.cpp
+++ Easy/217-ContainsDuplicate/main.cpp
@@ -6,15 +6,21 @@ class Solution
    public:
     bool containsDuplicate(std::vector<int>& nums)
     {
+        // Check if the size is less than or equal to 1.
+        // If it is we know there is no duplicates.
         if (nums.size() <= 1) return false;
 
+        // Sort the vecotr of numbers.
         std::sort(nums.begin(), nums.end());
 
+        // Now we can loop through a sorted vecotor strarting from the SECOND element.
         for (int i = 1; i < nums.size(); i++)
         {
-            if (nums[i] == nums[i - 1]) return true;
+            // Check if the current number matches the previous number.
+            if (nums[i] == nums[i - 1]) return true;  // If it does then there are ATLEAST two of the same numbers.
         }
 
+        // If not then there are no duplicates.
         return false;
     }
 };
